ca37ef947ee1ae77f580ab1fe36b3ea631225ec1
lefebvre1772@gmail.com <lefebvre1772@gmail.com>
2022-06-27 08:39:39 +0200

    added registration and login/logout

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticatorSecurityControllerAuthenticator extends AbstractLoginFormAuthenticator
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
@@ -50,7 +50,8 @@ class LoginFormAuthenticatorSecurityControllerAuthenticator extends AbstractLogi
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('index'));
+        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
